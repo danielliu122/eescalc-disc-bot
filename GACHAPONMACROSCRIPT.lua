76ea4a7a7a497ae6e5eac6b1fba219a012775f9b
Daniel Liu <daniel.liu@rutgers.edu>
2023-11-10 17:20:31 -0500

    Add files via upload

---
+++ GACHAPONMACROSCRIPT.lua
@@ -0,0 +1,361 @@
+function OnEvent(event, arg)
+    if event == "MOUSE_BUTTON_PRESSED" and arg == 3 then
+   		do
+
+			PressKey("enter");
+			Sleep(51);
+			ReleaseKey("enter");
+			Sleep(350);
+
+
+			PressKey("lshift");		
+			PressKey("2");
+			Sleep(51);
+			ReleaseKey("lshift");
+			Sleep(51);
+			ReleaseKey("2");
+			Sleep(350);
+
+
+			PressKey("s");
+			Sleep(51);
+			ReleaseKey("s");
+			Sleep(350);
+
+			PressKey("e");
+			Sleep(51);
+			ReleaseKey("e");
+			Sleep(350);
+
+
+			PressKey("l");
+			Sleep(51);
+			ReleaseKey("l");
+			Sleep(350);
+
+			PressKey("l");
+			Sleep(51);
+			ReleaseKey("l");
+			Sleep(350);
+
+			PressKey("enter");
+			Sleep(51);
+			ReleaseKey("enter");
+			Sleep(350);
+
+			PressKey("enter");
+			Sleep(51);
+			ReleaseKey("enter");
+			Sleep(350);
+
+
+
+			Sleep(50);
+			
+
+--- how many times??
+
+			for i=300, 1, -1
+				do
+
+				for i=17,1, -1
+					do
+						MoveMouseRelative(21583, 0)
+						Sleep(1);
+					end
+
+				Sleep(100);
+
+--- how many gachas?
+
+				for i=10,1,-1
+					do 
+
+						PressMouseButton(1);
+						Sleep(50);
+						ReleaseMouseButton(1);
+						Sleep(20);
+
+
+						MoveMouseTo(33502, 30794 )
+    	    					Sleep(50);
+       	 				PressMouseButton(1);
+      	 	   	 		Sleep(20);
+     	  				ReleaseMouseButton(1);
+       					Sleep(250);
+
+						PressKey("down");
+						Sleep(51);
+						ReleaseKey("down");
+						Sleep(99);
+
+						PressKey("enter");
+						Sleep(51);
+						ReleaseKey("enter");
+						Sleep(277);
+
+		
+
+						PressKey("enter");
+						Sleep(51);
+						ReleaseKey("enter");
+						Sleep(150);
+
+						PressKey("enter");
+						Sleep(51);
+						ReleaseKey("enter");
+						Sleep(51);
+
+
+						PressKey("up");
+						Sleep(51);
+						ReleaseKey("up");
+						Sleep(51);
+
+						PressKey("enter");
+						Sleep(51);
+						ReleaseKey("enter");
+						Sleep(51);
+
+						PressKey("enter");
+						Sleep(51);
+						ReleaseKey("enter");
+						Sleep(51);
+
+
+
+
+						for i=17,1,-1
+							do 
+							MoveMouseRelative(21583, 0)
+						end
+
+						PressMouseButton(1);
+						Sleep(50);
+						ReleaseMouseButton(1);
+					end
+
+
+
+
+
+
+--- end of gacha
+
+				MoveMouseTo(10000, 5345)
+        			Sleep(50);
+        			PressMouseButton(1);
+       	  	  	Sleep(20);
+       			ReleaseMouseButton(1);
+       			Sleep(51);
+	
+				PressKey("i");
+				Sleep(51);
+				ReleaseKey("i");
+				Sleep(51);
+
+				for i=17,1,-1
+					do 
+						MoveMouseRelative(21583, 0)
+					end
+
+				PressMouseButton(1);
+				Sleep(51);
+				ReleaseMouseButton(1);
+				Sleep(51);
+
+
+				MoveMouseTo(956, 5345)
+        			Sleep(50);
+        			PressMouseButton(1);
+       	  	  	Sleep(20);
+       			ReleaseMouseButton(1);
+       			Sleep(51);
+        			PressMouseButton(1);
+       	   	 	Sleep(20);
+       			ReleaseMouseButton(1);
+       			Sleep(250);
+		
+				for i=20,1,-1
+					do 
+					MoveMouseRelative(21583, 0)
+					Sleep(1);
+					end
+
+				PressMouseButton(1);
+				Sleep(50);
+				ReleaseMouseButton(1);
+
+		
+				MoveMouseTo(24418, 19557 )
+        			Sleep(50);
+        			PressMouseButton(1);
+       	 	   	Sleep(20);
+       			ReleaseMouseButton(1);
+       			Sleep(250);
+	
+				for i=17,1,-1
+					do 
+					MoveMouseRelative(21583, 0)
+					end
+
+				PressMouseButton(1);
+				Sleep(50);
+				ReleaseMouseButton(1);
+
+				MoveMouseTo( 22949, 29214 )
+        			Sleep(50);
+        			PressMouseButton(1);
+       	 	   	Sleep(20);
+       			ReleaseMouseButton(1);
+       			Sleep(51);
+
+--- SpamClick
+
+				for i=555,1,-1
+					do 
+        				PressMouseButton(1);
+       				ReleaseMouseButton(1);
+					Sleep(2);
+					PressMouseButton(1);	
+					ReleaseMouseButton(1);
+					PressKey("Enter");
+					ReleaseKey("Enter");
+       				end
+
+				Sleep(50);
+
+				for i=17,1,-1
+					do 
+					MoveMouseRelative(21583, 0)
+					end
+
+				PressMouseButton(1);
+				Sleep(50);
+				ReleaseMouseButton(1);
+
+
+--- set-up sell
+		
+
+
+
+				MoveMouseTo(25750, 19557)
+        			Sleep(50);
+        			PressMouseButton(1);
+       	 	   	Sleep(20);
+       			ReleaseMouseButton(1);
+       			Sleep(51);
+
+				for i=17,1,-1
+					do 
+					MoveMouseRelative(21583, 0)
+					end
+
+				PressMouseButton(1);
+				Sleep(50);
+				ReleaseMouseButton(1);
+
+				MoveMouseTo( 22949, 29214)
+        			Sleep(50);
+        			PressMouseButton(1);
+       	 	   	Sleep(20);
+       			ReleaseMouseButton(1);
+       			Sleep(51);
+
+--- spam-click
+
+				for i=50,1,-1
+					do 
+        				PressMouseButton(1);
+       				ReleaseMouseButton(1);
+					Sleep(2);
+					PressMouseButton(1);	
+					ReleaseMouseButton(1);
+					PressKey("Enter");
+					ReleaseKey("Enter");
+       				end
+
+				Sleep(50);
+
+
+				for i=17,1,-1
+					do 
+					MoveMouseRelative(21583, 0)
+					end
+
+				PressMouseButton(1);
+				Sleep(50);
+				ReleaseMouseButton(1);
+
+--- EES
+				MoveMouseTo( 17519, 24841 )
+        			Sleep(50);
+        			PressMouseButton(1);
+       	 	   	Sleep(20);
+       			ReleaseMouseButton(1);
+       			Sleep(51);
+
+				for i=1,1,-1
+					do 
+        				PressMouseButton(1);
+       				ReleaseMouseButton(1);
+					Sleep(2);
+					PressMouseButton(1);	
+					ReleaseMouseButton(1);
+					PressKey("Enter");
+					ReleaseKey("Enter");
+       				end
+
+
+				Sleep(50);
+
+				PressKey("Enter");
+				Sleep(50);
+				ReleaseKey("Enter");
+				Sleep(50);
+
+
+				for i=17,1,-1
+					do 
+					MoveMouseRelative(21583, 0)
+					end
+
+				PressMouseButton(1);
+				Sleep(50);
+				ReleaseMouseButton(1);
+			
+
+--- leave store
+
+				MoveMouseTo(20422, 14638)
+        			Sleep(50);
+        			PressMouseButton(1);
+       	  	  	Sleep(20);
+       			ReleaseMouseButton(1);
+       			Sleep(51);
+
+				Sleep(150);
+
+				PressKey("i");
+				Sleep(51);
+				ReleaseKey("i");
+				Sleep(51);
+
+				for i=17,1,-1
+					do 
+					MoveMouseRelative(21583, 0)
+					end
+
+				PressMouseButton(1);
+				Sleep(50);
+				ReleaseMouseButton(1);
+				Sleep(50);
+
+
+
+			end
+
+		end
+	end
+end	
